0dd2ed1ccf83c2e198baa298902dec4ad45385c1
BreezeXrf <m13810712595@163.com>
2023-05-26 12:02:32 +0800

    feat(xrf): 按照不同的标准排列post

--- lib/pages/post_list.dart
+++ lib/pages/post_list.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:thumuht/components/post_list.dart';
+import 'package:thumuht/model/gql/graphql_api.dart';
 
 class PostListPage extends StatefulWidget {
-  const PostListPage({super.key});
-
+  PostListPage({super.key, required this.orderBy});
+  PostOrderBy orderBy;
   @override
   State<PostListPage> createState() => _PostListPageState();
 }
 
 class _PostListPageState extends State<PostListPage> {
   @override
-  Widget build(BuildContext context) => trueList(context);
+  // Widget build(BuildContext context) => trueList(context, widget.orderBy);
+  Widget build(BuildContext context) {
+    //   return TabBarView(
+    //     children: [
+    //       trueList(context, PostOrderBy.createdAt),
+    //       trueList(context, PostOrderBy.like),
+    //       trueList(context, PostOrderBy.commentsNum),
+    //     ],
+    //   );
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: const TabBar(
+          tabs: [
+            Tab(text: '最新'),
+            Tab(text: '最热'),
+            Tab(text: '评论'),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            trueList(context, PostOrderBy.createdAt),
+            trueList(context, PostOrderBy.like),
+            trueList(context, PostOrderBy.commentsNum),
+          ],
+        ),
+      ),
+    );
+  }
 }
